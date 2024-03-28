b2fbca95356c55b1222319d15f1af21ed2b8c9ca
Lennonnag <lennon_nagata@hotmail.com>
2024-03-28 18:01:04 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
+import React, { useState } from "react";
 
 const App = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const handleAddNumber = (number) => {
+    setCurrentNumber((prev) => `${number}${prev === '0' ? '' : prev}`);
+  };
+
+  const handleOnClear = () => {
+    setCurrentNumber("0");
+  };
   return (
     <Container className="p-2">
       <h1 className="header">Calculadora React-Bootstrap</h1>
       <Container className="rounded border" style={{ width: "20rem" }}>
-        <Form.Control className="m-2" />
+        <Form.Control className="m-2 text-end" value={currentNumber} />
         <Stack direction="horizontal" gap={2}>
-          <Button className="mt-2 ms-auto" variant="primary" style={{ width: "auto" }}>
+          <Button
+            className="mt-2 ms-auto"
+            variant="primary"
+            style={{ width: "auto" }}
+            onClick={handleOnClear}
+          >
             Clear
           </Button>
           <Button className="mt-2" variant="primary">
@@ -24,13 +38,25 @@ const App = () => {
           </Button>
         </Stack>
         <Stack direction="horizontal" gap={2}>
-          <Button className="mt-2 ms-auto" variant="primary">
+          <Button
+            className="mt-2 ms-auto"
+            variant="primary"
+            onClick={() => handleAddNumber("7")}
+          >
             7
           </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("8")}
+          >
             8
           </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("9")}
+          >
             9
           </Button>
           <Button className="mt-2" variant="primary">
@@ -38,13 +64,25 @@ const App = () => {
           </Button>
         </Stack>
         <Stack direction="horizontal" gap={2}>
-          <Button className="mt-2 ms-auto" variant="primary">
-            3
-          </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2 ms-auto"
+            variant="primary"
+            onClick={() => handleAddNumber("4")}
+          >
             4
           </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("5")}
+          >
+            5
+          </Button>
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("6")}
+          >
             6
           </Button>
           <Button className="mt-2" variant="primary">
@@ -52,17 +90,39 @@ const App = () => {
           </Button>
         </Stack>
         <Stack direction="horizontal" gap={2}>
-          <Button className="mt-2 ms-auto" variant="primary">
+          <Button
+            className="mt-2 ms-auto"
+            variant="primary"
+            onClick={() => handleAddNumber("1")}
+          >
             1
           </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("2")}
+          >
             2
           </Button>
-          <Button className="mt-2" variant="primary">
+          <Button
+            className="mt-2"
+            variant="primary"
+            onClick={() => handleAddNumber("3")}
+          >
             3
           </Button>
           <Button className="mt-2" variant="primary">
             =
+          </Button>
+        </Stack>
+
+        <Stack direction="horizontal" gap={2}>
+          <Button
+            className="mt-2 ms-auto"
+            variant="primary"
+            onClick={() => handleAddNumber("0")}
+          >
+            0
           </Button>
         </Stack>
       </Container>
